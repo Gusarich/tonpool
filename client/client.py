64b595d4e6f470df36a61b74beae581eb94cce4c
Gusarich <danielsedovzzz@gmail.com>
2021-09-21 08:45:13 +0300

    f

--- client/client.py
+++ client/client.py
@@ -1,16 +1,21 @@
-from subprocess import check_output
+from subprocess import check_output, PIPE
 import requests
 
 
 POOL_SERVER = 'http://localhost:8080/'
 
 
-def mine(threads, difficulty_level):
+def get_params(threads, difficulty_level):
     params = requests.get(f'{POOL_SERVER}params?difficulty={difficulty_level}').json()
     miner, seed, complexity, giver = params
-    print(miner, seed, complexity, giver)
     cmd = f'pow-miner -vv -w{threads} -t20 {miner} {seed} {complexity} 999999999999999 {giver} diff{difficulty_level}_{randint(1000000000, 9999999999)}.boc'
-    print(cmd)
+    return cmd
+
+
+def mine(threads, difficulty_level):
+    cmd = get_params(threads, difficulty_level)
+    result = check_output(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+    print([result])
 
 
 mine(8, 1)
