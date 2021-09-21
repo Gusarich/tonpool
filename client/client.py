aa31ead6c4bcd7e4cda54fab0529f1f78d3c235e
Gusarich <danielsedovzzz@gmail.com>
2021-09-21 09:04:55 +0300

    params fix in client

--- client/client.py
+++ client/client.py
@@ -8,7 +8,7 @@ POOL_SERVER = 'http://3.137.150.46/'
 
 def get_params(threads, difficulty_level):
     params = requests.get(f'{POOL_SERVER}params?difficulty={difficulty_level}').json()
-    miner, seed, complexity, giver = params
+    miner, seed, complexity, giver = params['miner'], params['seed'], params['complexity'], params['giver']
     cmd = f'pow-miner -vv -w{threads} -t20 {miner} {seed} {complexity} 999999999999999 {giver} diff{difficulty_level}_{randint(1000000000, 9999999999)}.boc'
     return cmd
 
